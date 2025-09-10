5a6a2ffdd077fb2963691b1ff3402e3693b10ba9
ikorb0197 <ikorb0197@floridapoly.edu>
2025-09-10 13:34:18 -0400

    Functions

---
+++ 07_functions/functions.py
@@ -0,0 +1,21 @@
+# Function that creates a message
+def create_message(prefix, *args):
+    s = str(prefix) + ": "
+    for arg in args:
+        s += str(arg) + " "
+    print(s)
+
+def create_mes(*args, **kwargs):
+    # Find the key in kwargs
+    separator = kwargs.get("separator", " ")
+    s = ""
+    for arg in args:
+        s += str(arg) + separator
+    
+    return s[:-1]
+
+# Call a function
+create_message("Status", "Temp", 25, "Degree")
+create_message("Alert", "System", "is", "online")
+
+print(create_mes("a", "b", separator = "-"))

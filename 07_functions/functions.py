d74219f510c35e9cea8fcb59fc92de6ac4dd7464
ikorb0197 <ikorb0197@floridapoly.edu>
2025-09-15 17:53:56 -0400

    Functions

--- 07_functions/functions.py
+++ 07_functions/functions.py
@@ -8,14 +8,17 @@ def create_message(prefix, *args):
 def create_mes(*args, **kwargs):
     # Find the key in kwargs
     separator = kwargs.get("separator", " ")
-    s = ""
-    for arg in args:
-        s += str(arg) + separator
-    
-    return s[:-1]
+    prefix = kwargs.get("prefix", "Q")
+    suffix = kwargs.get("suffix", 1)
+    msg = prefix + separator.join(args) + suffix
+
+    return msg
 
 # Call a function
 create_message("Status", "Temp", 25, "Degree")
 create_message("Alert", "System", "is", "online")
 
-print(create_mes("a", "b", separator = "-"))
+print(create_mes("a", "b", separator = "-", prefix = "start", suffix = "end"))
+
+# Qpy-th-on1
+print(create_mes("py", "th", "on", separator = "-", prefix = "Q", suffix = "1"))

599d05ced52e5796fc9bb8388beb0b2326ed686f
ikorb0197 <ikorb0197@floridapoly.edu>
2025-09-27 00:41:00 -0400

    Main

--- 10_decorators/main.py
+++ 10_decorators/main.py
@@ -1,3 +1,8 @@
 from measure import f2
+# import string_utils
+from string_utils import make_palindrome
 
 f2(1.5)
+print()
+# print(string_utils.make_palindrome(string_utils.DEFAULT_TEXT))
+print(make_palindrome("buh"))

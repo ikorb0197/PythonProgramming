fd8f3709f48b4f70d12bcde2f25f0c93045d30c7
ikorb0197 <ikorb0197@floridapoly.edu>
2025-10-01 01:56:45 -0400

    Static methods

--- 13_static_methods/static_methods.py
+++ 13_static_methods/static_methods.py
@@ -6,9 +6,17 @@ class PathUtils:
     
     ### TODO
     # static get_directory - extracts directory path
+    @staticmethod
+    def get_directory(path):
+        return path[:path.rfind("/")] if "/" in path else ""
     # static get_basename - extracts the file or directory name
+    def get_basename(file):
+        return file[file.rfind("/") + 1 : file.rfind(".")] if "/" and "." in file else ""
 
 # Usage
 print(PathUtils.get_extension("1.txt"))
 print(PathUtils.get_extension("image.png"))
 print(PathUtils.get_extension("file"))
+
+print(PathUtils.get_directory("Users/ikorb/school/COP2080/PythonProgramming/13_static_methods/static_methods.py"))
+print(PathUtils.get_basename("Users/ikorb/school/COP2080/PythonProgramming/13_static_methods/static_methods.py"))

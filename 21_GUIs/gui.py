124f3ed336b60a84fd2f66c90e25cae32b2e51fa
ikorb0197 <ikorb0197@floridapoly.edu>
2025-11-03 13:50:27 -0500

    nicegui

--- 21_GUIs/gui.py
+++ 21_GUIs/gui.py
@@ -1,4 +1,34 @@
 from nicegui import ui
 
-ui.label("Hello, NiceGUI!")
-ui.run()
+ui.label("Welcome to NiceGUI!").style("color: purple; font-size: 40px")
+
+# Create a greeting
+
+def greet():
+    name = input_field.value.strip()
+    msg = f"Hello, {name or "stranger"}"
+    ui.notify(msg) # creates a popup
+
+input_field = ui.input("Enter your name")
+ui.button("Greet Me!", color="purple", on_click=greet)
+
+# Create a counter
+class State:
+    count = 0
+
+count_label = ui.label("Count: 0")
+
+def add_one():
+    State.count += 1
+    count_label.text = f"Count: {State.count}"
+
+ui.button("Add one", color="green", on_click=add_one)
+
+# TODO
+# Create a Reset button that resets the count to 0
+def reset_count():
+    State.count = 0
+    count_label.text = f"Count: {State.count}"
+ui.button("Reset", color="red", on_click=reset_count)
+
+ui.run(title="Intro to nicegui")
